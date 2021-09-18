e38f444607c55d135e1dade865a0ee2ec29ee865
nasifalnahian <nasifalnahian393@gmail.com>
2021-09-18 15:34:03 +0600

    code optimize

--- account.js
+++ account.js
@@ -1,64 +1,34 @@
-function getinputvalue() {
-    const depositeinput=document.getElementById('deposite-input-box');
+function getinputvalue(inputid) {
+    const depositeinput=document.getElementById(inputid);
     const depositeammountText = depositeinput.value;
     const depositeammount =parseFloat(depositeammountText);
 
     depositeinput.value='';
-    return depositeammount;
-
-
-   
+    return depositeammount;  
 };
-
-
 document.getElementById('deposite-button').addEventListener('click',function()
 {
     // button connect
     // console.log('work');
-
     // const depositeinput=document.getElementById('deposite-input-box');
     // const depositeammountText = depositeinput.value;
     // const depositeammount =parseFloat(depositeammountText);
     // console.log(depositeammount);
-
-
-
-
- 
-const depositeammount = getinputvalue();
-
+const depositeammount = getinputvalue('deposite-input-box');
     // input box data collect 
     // depositeammonut =  what ever i input in box
-
-
 const depositebalance = document.getElementById('deposite-balance');
-
 // deposite balance data taken
-
 console.log(depositebalance.innerText);
-
-
 const predepositebalanceText=depositebalance.innerText;
 const predepositebalance=parseFloat(predepositebalanceText);
 // input data send to deposite data
-
 const currentdeposite =parseFloat(predepositebalance)+depositeammount;
 // console.log(newdepositebalance);
- 
 depositebalance.innerText = currentdeposite;
-
 // clear input box
 // depositeinput.value='';
-
-
-
-
-
-// 
 // Update Totall Balance
-
-
-
 const balancetotal = document.getElementById('total-balance');
 
 const balancetotalText =balancetotal.innerText;
@@ -68,95 +38,40 @@ const prebalancetotal = parseFloat(balancetotalText);
 const newbalancetotl = predepositebalance + depositeammount;
 
 balancetotal.innerText= newbalancetotl;
-
-
-
-
 // update Withdraw balance
-
-
-
-
-
-
-
-
-
-
-
 });
-
-
-
-
 // document.getElementById('withdraw-button').addEventListener('click',function()
 // {
 //     // console.log('adsd');
-
 //     const withdrawinput = document.getElementById('deposite-input-box');
 //     const withdrawammountText = withdrawinput.value;
-
-//     const newwithdrawammount= parseFloat(withdrawammountText);
-   
-  
+//     const newwithdrawammount= parseFloat(withdrawammountText); 
 // })
-
 document.getElementById('withdraw-button').addEventListener('click',function()
 {
     // button connect
     // console.log('work');
-
-    const withdrawinput=document.getElementById('withdraw-input-box');
-    const withdrawammountText = withdrawinput.value;
-    const withdrawammount =parseFloat(withdrawammountText);
+    // const withdrawinput=document.getElementById('withdraw-input-box');
+    // const withdrawammountText = withdrawinput.value;
+    // const withdrawammount =parseFloat(withdrawammountText);
     // console.log(withdrawammount);
-
     // input box data collect 
     // withdrawammonut =  what ever i input in box
-
-
+const  withdrawammount = getinputvalue('withdraw-input-box');
 const withdrawbalance = document.getElementById('withdraw-balance');
-
 // withdraw balance data taken
-
 console.log(withdrawbalance.innerText);
-
-
 const prewithdrawbalanceText=withdrawbalance.innerText;
 const prewithdrawbalance=parseFloat(prewithdrawbalanceText);
 // input data send to withdraw data
-
 const currentwithdraw =parseFloat(prewithdrawbalance)+ withdrawammount;
 // console.log(newwithdrawbalance);
- 
 withdrawbalance.innerText = currentwithdraw;
-
 // clear input box
-withdrawinput.value='';
-
-
-
-
-
-
-
+// withdrawinput.value='';
 const balancetotal = document.getElementById('total-balance');
-
 const balancetotalText =balancetotal.innerText;
-
 const prebalancetotal = parseFloat(balancetotalText);
-
 const newbalancetotl = prebalancetotal - withdrawammount;
-
 balancetotal.innerText= newbalancetotl;
-
-
-
-
-
-
-
-
-
-
 });
